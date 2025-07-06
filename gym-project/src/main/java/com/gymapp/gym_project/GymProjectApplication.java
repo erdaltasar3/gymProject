efdf9b2eb3d684104edfec3b21a3824c91a964fe
Erdal <erdaltasar24@gmail.com>
2025-07-06 18:15:40 +0300

    first commit

---
+++ gym-project/src/main/java/com/gymapp/gym_project/GymProjectApplication.java
@@ -0,0 +1,13 @@
+package com.gymapp.gym_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymProjectApplication.class, args);
+	}
+
+}
---
+++ gym-project/src/test/java/com/gymapp/gym_project/GymProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gymapp.gym_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
